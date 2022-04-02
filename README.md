efe2b925ce475d81b720796a04556674c3e0b927
rajesh-rahul <rahul-rajesh@outlook.com>
2022-04-02 15:29:03 -0700

    squash commits

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # final-project
+
+parameters for twieet field in call:
+[attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,non_public_metrics,organic_metrics,possibly_sensitive,promoted_metrics,public_metrics,referenced_tweets,reply_settings,source,text,withheld]
---
+++ getTweets.py
@@ -0,0 +1,23 @@
+from searchtweets import ResultStream, load_credentials, collect_results,gen_request_parameters
+# import pandas as pd
+# import numpy as np
+
+import os, json
+
+v2_search_args = load_credentials(
+    ".twitter_keys.yaml", yaml_key="search_tweets_v2", env_overwrite=False)
+
+# testing with a sandbox account
+query = gen_request_parameters("covid",tweet_fields = "created_at,geo",
+ results_per_call=10,granularity=None)
+
+tweets = collect_results(query, max_tweets=1,
+                         result_stream_args=v2_search_args)
+
+print (tweets)
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(tweets, f, ensure_ascii=False, indent=4)
+
+
+
